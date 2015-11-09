028cbcfa83b4d5c206be960684ab37cc44af5112
Niklas Keller <me@kelunik.com>
2015-11-09 09:31:50 +0100

    Fix Typo

--- lib/AcmeResource.php
+++ lib/AcmeResource.php
@@ -5,7 +5,7 @@ namespace Kelunik\Acme;
 class AcmeResource {
     const NEW_REGISTRATION = "new-reg";
     const RECOVER_REGISTRATION = "recover-reg";
-    const NEW_AUTHORIZATION = "new authz";
+    const NEW_AUTHORIZATION = "new-authz";
     const NEW_CERTIFICATE = "new-cert";
     const REVOKE_CERTIFICATE = "revoke-cert";
     const REGISTRATION = "reg";
