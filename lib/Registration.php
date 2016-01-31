130bb00172a7265b2ceb14aa5b2986799cfb2e16
Daniel Speichert <daniel@speichert.pl>
2016-01-31 11:59:40 +0100

    Update Registration.php to include more information

--- lib/Registration.php
+++ lib/Registration.php
@@ -8,13 +8,37 @@ namespace Kelunik\Acme;
  * @package Kelunik\Acme
  */
 class Registration {
+    private $location;
     private $contact;
+    private $agreement;
+    private $authorizations;
+    private $certificates;
 
-    public function __construct(array $contact) {
+    public function __construct(string $location, array $contact, string $agreement = null, array $authorizations = [], array $certificates = []) {
+        $this->location = $location;
         $this->contact = $contact;
+        $this->agreement = $agreement;
+        $this->authorizations = $authorizations;
+        $this->certificates = $certificates;
     }
 
+    public function getLocation() {
+        return $this->location;
+    }
+    
     public function getContact() {
         return $this->contact;
+    }
+    
+    public function getAgreement() {
+        return $this->agreement;
+    }
+    
+    public function getAuthorizations() {
+        return $this->authorizations;
+    }
+    
+    public function getCertificates() {
+        return $this->certificates;
     }
 }
