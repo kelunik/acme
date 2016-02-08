503499b207e4b04f63271c25d55afc0bc341d63e
Niklas Keller <me@kelunik.com>
2016-02-08 11:50:15 +0100

    Add LICENSE and first dummy test

---
+++ test/SelfVerifyTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kelunik\Acme;
+
+class SelfVerifyTest extends \PHPUnit_Framework_TestCase {
+    public function testIgnoresWrongPeerName() {
+        $this->assertTrue(true);
+    }
+}
