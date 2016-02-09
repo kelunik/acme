ea76560331d53e38cf833ae160b2c7856b5c55eb
Niklas Keller <me@kelunik.com>
2016-02-09 20:31:12 +0100

    Add selfVerify tests

--- test/SelfVerifyTest.php
+++ test/SelfVerifyTest.php
@@ -2,8 +2,59 @@
 
 namespace Kelunik\Acme;
 
+use Amp\Artax\Client;
+use Amp\Artax\Response;
+
 class SelfVerifyTest extends \PHPUnit_Framework_TestCase {
-    public function testIgnoresWrongPeerName() {
-        $this->assertTrue(true);
+    /**
+     * @test
+     */
+    public function ignoresWrongPeerName() {
+        \Amp\run(function() {
+            $http = new Client();
+
+            $keyPair = (new OpenSSLKeyGenerator())->generate();
+            $client = new AcmeClient("https://acme-staging.api.letsencrypt.org/directory", $keyPair);
+            $service = new AcmeService($client, $keyPair);
+
+            $token = "invalid-common-name";
+
+            /** @var Response $payloadResponse */
+            $payloadResponse = yield $http->request("http://blog.kelunik.com/robots.txt");
+            $payload = trim($payloadResponse->getBody());
+
+            try {
+                yield $service->selfVerify("kelunik.com", $token, $payload);
+                $this->assertTrue(true);
+            } catch (\Throwable $e) {
+                $this->fail("Didn't ignore invalid common name. " . $e);
+            } catch (\Exception $e) {
+                $this->fail("Didn't ignore invalid common name. " . $e);
+            }
+
+            \Amp\stop();
+        });
+    }
+
+    /**
+     * @test
+     * @expectedException \Kelunik\Acme\AcmeException
+     * @expectedExceptionMessage selfVerify failed
+     */
+    public function failsOnDiffPayload() {
+        \Amp\run(function() {
+            $keyPair = (new OpenSSLKeyGenerator())->generate();
+            $client = new AcmeClient("https://acme-staging.api.letsencrypt.org/directory", $keyPair);
+            $service = new AcmeService($client, $keyPair);
+
+            $token = "abcdef";
+
+            /** @var Response $payloadResponse */
+            $payload = "foobar";
+
+            yield $service->selfVerify("kelunik.com", $token, $payload);
+
+            \Amp\stop();
+        });
     }
 }
