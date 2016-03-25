dc26c0f62d823bc78afb2eafd785f9bbf55af045
Niklas Keller <me@kelunik.com>
2016-03-25 21:37:07 +0100

    Add more tests for Http01Verification

--- test/Http01VerificationTest.php
+++ test/Http01VerificationTest.php
@@ -6,63 +6,64 @@ use Amp\Artax\Client;
 use Amp\Artax\Response;
 
 class Http01VerificationTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var AcmeService
+     */
+    private $acme;
+
     public function setUp() {
         \Amp\reactor(\Amp\driver());
         \Amp\Dns\resolver(\Amp\Dns\driver());
+
+        $keyPair = (new OpenSSLKeyGenerator())->generate();
+        $client = new AcmeClient("https://acme-staging.api.letsencrypt.org/directory", $keyPair);
+        $this->acme = new AcmeService($client);
     }
 
     /**
      * @test
      */
     public function ignoresWrongPeerName() {
-        \Amp\run(function () {
-            $http = new Client();
+        /** @var Response $payloadResponse */
+        $payloadResponse = \Amp\wait((new Client)->request("http://blog.kelunik.com/robots.txt"));
+        $payload = trim($payloadResponse->getBody());
 
-            $keyPair = (new OpenSSLKeyGenerator())->generate();
-            $client = new AcmeClient("https://acme-staging.api.letsencrypt.org/directory", $keyPair);
-            $service = new AcmeService($client);
-
-            $token = "invalid-common-name";
-
-            /** @var Response $payloadResponse */
-            $payloadResponse = (yield $http->request("http://blog.kelunik.com/robots.txt"));
-            $payload = trim($payloadResponse->getBody());
-
-            try {
-                yield $service->verifyHttp01Challenge("kelunik.com", $token, $payload);
-                $this->assertTrue(true);
-            } catch (\Throwable $e) {
-                $this->fail("Didn't ignore invalid common name. " . $e);
-            } catch (\Exception $e) {
-                $this->fail("Didn't ignore invalid common name. " . $e);
-            } finally {
-                \Amp\stop();
-            }
-        });
+        \Amp\wait($this->acme->verifyHttp01Challenge("kelunik.com", "invalid-common-name", $payload));
     }
 
     /**
      * @test
+     * @expectedException \Kelunik\Acme\AcmeException
+     * @expectedExceptionMessage selfVerify failed
      */
     public function failsOnWrongPayload() {
-        \Amp\run(function () {
-            $keyPair = (new OpenSSLKeyGenerator())->generate();
-            $client = new AcmeClient("https://acme-staging.api.letsencrypt.org/directory", $keyPair);
-            $service = new AcmeService($client);
+        \Amp\wait($this->acme->verifyHttp01Challenge("kelunik.com", "abcdef", "foobar"));
+    }
 
-            $token = "abcdef";
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage domain must be of type string
+     */
+    public function failsIfDomainNotString() {
+        \Amp\wait($this->acme->verifyHttp01Challenge(null, "abcdef", "foobar"));
+    }
 
-            /** @var Response $payloadResponse */
-            $payload = "foobar";
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage token must be of type string
+     */
+    public function failsIfTokenNotString() {
+        \Amp\wait($this->acme->verifyHttp01Challenge("kelunik.com", null, "foobar"));
+    }
 
-            try {
-                yield $service->verifyHttp01Challenge("kelunik.com", $token, $payload);
-                $this->assertTrue(true);
-            } catch (AcmeException $e) {
-                $this->assertContains("selfVerify failed", $e->getMessage());
-            } finally {
-                \Amp\stop();
-            }
-        });
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage payload must be of type string
+     */
+    public function failsIfPayloadNotString() {
+        \Amp\wait($this->acme->verifyHttp01Challenge("kelunik.com", "abcdef", null));
     }
 }
