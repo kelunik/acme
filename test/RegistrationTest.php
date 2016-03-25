0920d7a4bb1508e0ce5103c06a5deb9a2e37213e
Niklas Keller <me@kelunik.com>
2016-03-25 20:53:21 +0100

    Add KeyPair and Registration Tests

---
+++ test/RegistrationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kelunik\Acme;
+
+class RegistrationTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @param string      $location URI of the registration object
+     * @param array       $contact all contacts registered with the server
+     * @param string|null $agreement URI to the agreement, if agreed
+     * @param string      $authorizations URI to retrieve authorizations
+     * @param string      $certificates URI to retrieve certificates
+     * @dataProvider provideSuccessArgs
+     * @test
+     */
+    public function success($location, array $contact = [], $agreement = null, $authorizations = null, $certificates = null) {
+        $reg = new Registration($location, $contact, $agreement, $authorizations, $certificates);
+
+        $this->assertSame($location, $reg->getLocation());
+        $this->assertSame($contact, $reg->getContact());
+        $this->assertSame($agreement, $reg->getAgreement());
+        $this->assertSame($authorizations, $reg->getAuthorizations());
+        $this->assertSame($certificates, $reg->getCertificates());
+    }
+
+    public function provideSuccessArgs() {
+        $server = "https://acme-v01.api.letsencrypt.org/directory";
+
+        return [
+            [$server],
+            [$server, []],
+            [$server, ["mailto:me@example.com"]],
+            [$server, ["mailto:me@example.com"], null, "{$server}/authz"],
+            [$server, ["mailto:me@example.com"], null, "{$server}/authz", "{$server}/certs"],
+            [$server, ["mailto:me@example.com"], "{$server}/tos", "{$server}/authz", "{$server}/certs"],
+        ];
+    }
+}
