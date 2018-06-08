26bfdbe167d295c046240b419a78a0cb6b3f9c1d
Niklas Keller <me@kelunik.com>
2018-06-08 14:28:30 +0200

    Run downgraded boulder version

--- test/.run-boulder.sh
+++ test/.run-boulder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker pull acmephp/testing-ca:1.0.0
-docker run -d --name boulder --net host acmephp/testing-ca
+docker run -d --name boulder --net host acmephp/testing-ca:1.0.0
