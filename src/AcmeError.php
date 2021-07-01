bce47a0b530999b4fdf34342ae9734426c314836
Niklas Keller <me@kelunik.com>
2021-07-01 19:56:48 +0200

    Update AcmeError constants

--- src/AcmeError.php
+++ src/AcmeError.php
@@ -18,147 +18,146 @@ namespace Kelunik\Acme;
 final class AcmeError
 {
     /**
-     * Non existing account:
+     * Account does not exist:
      * (RFC8555) The request specified an account that does not exist.
      */
-    public const TYPE_ACCOUNT_NON_EXISTS = 'accountDoesNotExist';
+    public const ACCOUNT_DOES_NOT_EXIST = 'accountDoesNotExist';
 
     /**
      * Already revoked:
      * (RFC8555) The request specified a certificate to be revoked that has already been revoked.
      */
-    public const TYPE_ALREADY_REVOKED = 'alreadyRevoked';
+    public const ALREADY_REVOKED = 'alreadyRevoked';
 
     /**
      * Bad CSR:
      * (RFC8555) The CSR is unacceptable (e.g., due to a short key).
      */
-    public const TYPE_BAD_CSR = 'badCSR';
+    public const BAD_CSR = 'badCSR';
 
     /**
      * Bad replay nonce:
      * (RFC8555) The client sent an unacceptable anti-replay nonce.
      */
-    public const TYPE_BAD_NONCE = 'badNonce';
+    public const BAD_NONCE = 'badNonce';
 
     /**
      * Bad public key:
      * (RFC8555) The JWS was signed by a public key the server does not support.
      */
-    public const TYPE_BAD_PUBLIC_KEY = 'badPublicKey';
+    public const BAD_PUBLIC_KEY = 'badPublicKey';
 
     /**
      * Bad revocation reason:
      * (RFC8555) The revocation reason provided is not allowed by the server.
      */
-    public const TYPE_BAD_REVOCATION_REASON = 'badRevocationReason';
+    public const BAD_REVOCATION_REASON = 'badRevocationReason';
 
     /**
      * Bad signature algorithm:
      * (RFC8555) The JWS was signed with an algorithm the server does not support.
      */
-    public const TYPE_BAD_SIGNATURE_ALG = 'badSignatureAlgorithm';
+    public const BAD_SIGNATURE_ALGORITHM = 'badSignatureAlgorithm';
 
     /**
-     * caa:
+     * Forbidden by Certification Authority Authorization (CAA):
      * (RFC8555) Certification Authority Authorization (CAA) records forbid the CA from issuing a certificate.
      */
-    public const TYPE_CAA = 'caa';
+    public const CAA = 'caa';
 
     /**
      * Compound:
      * (RFC8555) Specific error conditions are indicated in the "subproblems" array.
      */
-    public const TYPE_COMPOUND = 'compound';
+    public const COMPOUND = 'compound';
 
     /**
      * Connection error:
      * (RFC8555) The server could not connect to a validation target.
      */
-    public const TYPE_CONNECTION = 'connection';
+    public const CONNECTION = 'connection';
 
     /**
      * Error related to DNS:
      * (RFC8555) There was a problem with a DNS query during identifier validation.
      */
-    public const TYPE_DNS = 'dns';
+    public const DNS = 'dns';
 
     /**
      * External account required:
      * (RFC8555) The request must include a value for the "externalAccountBinding" field.
-     *
      */
-    public const TYPE_EXTERNAL_ACCOUNT_REQUIRED = 'externalAccountRequired';
+    public const EXTERNAL_ACCOUNT_REQUIRED = 'externalAccountRequired';
 
     /**
      * Incorrect response:
      * (RFC8555) Response received didn't match the challenge's requirements.
      */
-    public const TYPE_INCORRECT_RESPONSE = 'incorrectResponse';
+    public const INCORRECT_RESPONSE = 'incorrectResponse';
 
     /**
      * Invalid contact:
      * (RFC8555) A contact URL for an account was invalid.
      */
-    public const TYPE_INVALID_CONTACT = 'invalidContact';
+    public const INVALID_CONTACT = 'invalidContact';
 
     /**
      * Malformed request:
      * (RFC8555) The request message was malformed.
      */
-    public const TYPE_MALFORMED = 'malformed';
+    public const MALFORMED = 'malformed';
 
     /**
      * Order not ready:
      * (RFC8555) The request attempted to finalize an order that is not ready to be finalized.
      */
-    public const TYPE_ORDER_NOT_READY = 'orderNotReady';
+    public const ORDER_NOT_READY = 'orderNotReady';
 
     /**
      * Rate limit exceeded:
      * (RFC8555) The request exceeds a rate limit.
      */
-    public const TYPE_RATE_LIMITED = 'rateLimited';
+    public const RATE_LIMITED = 'rateLimited';
 
     /**
-     * Rejected identifier: The server will not issue certificates for the identifier
-     * (RFC8555):.
+     * Rejected identifier:
+     * (RFC8555) The server will not issue certificates for the identifier.
      */
-    public const TYPE_REJECTED_IDENTIFIER = 'rejectedIdentifier';
+    public const REJECTED_IDENTIFIER = 'rejectedIdentifier';
 
     /**
      * Internal server error:
      * (RFC8555) The server experienced an internal error.
      */
-    public const TYPE_SERVER_INTERNAL = 'serverInternal';
+    public const SERVER_INTERNAL = 'serverInternal';
 
     /**
      * TLS error.
      * (RFC8555) The server received a TLS error during validation.
      */
-    public const TYPE_TLS = 'tls';
+    public const TLS = 'tls';
 
     /**
      * Unauthorized:
      * (RFC8555) The client lacks sufficient authorization.
      */
-    public const TYPE_UNAUTHORIZED = 'unauthorized';
+    public const UNAUTHORIZED = 'unauthorized';
 
     /**
      * Unsupported contact:
      * (RFC8555) A contact URL for an account used an unsupported protocol scheme.
      */
-    public const TYPE_UNSUPPORTED_CONTACT = 'unsupportedContact';
+    public const UNSUPPORTED_CONTACT = 'unsupportedContact';
 
     /**
      * Unsupported identifier:
      * (RFC8555) An identifier is of an unsupported type.
      */
-    public const TYPE_UNSUPPORTED_IDENTIFIER = 'unsupportedIdentifier';
+    public const UNSUPPORTED_IDENTIFIER = 'unsupportedIdentifier';
 
     /**
      * User action required:
      * (RFC8555) Visit the "instance" URL and take actions specified there.
      */
-    public const TYPE_USER_ACTION_REQUIRED = 'userActionRequired';
+    public const USER_ACTION_REQUIRED = 'userActionRequired';
 }
