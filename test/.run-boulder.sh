03913646e5fb9327cf95f6375ebf5ebc0e57597f
Niklas Keller <me@kelunik.com>
2022-08-10 22:44:41 +0200

    Fix running pebble on Apple Silicon

--- test/.run-boulder.sh
+++ test/.run-boulder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker pull letsencrypt/pebble
-docker run -d -e 'PEBBLE_VA_NOSLEEP=1' -p 15000:15000 -p 14000:14000 --name pebble letsencrypt/pebble
+docker run --platform linux/amd64 -d -e 'PEBBLE_VA_NOSLEEP=1' -p 15000:15000 -p 14000:14000 --name pebble letsencrypt/pebble
