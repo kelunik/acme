3c81b430e3b6f33502d9682fe69ea5a5a4a80f65
Niklas Keller <me@kelunik.com>
2025-08-09 20:42:57 +0200

    Add issuance test

--- test/AcmeServiceTest.php
+++ test/AcmeServiceTest.php
@@ -9,6 +9,9 @@ use Amp\PHPUnit\AsyncTestCase;
 use Amp\Socket\ClientTlsContext;
 use Amp\Socket\ConnectContext;
 use Kelunik\Acme\Crypto\RsaKeyGenerator;
+use Kelunik\Acme\Csr\OpensslCsrGenerator;
+use Kelunik\Acme\Protocol\Authorization;
+use Kelunik\Acme\Protocol\Challenge;
 
 final class AcmeServiceTest extends AsyncTestCase
 {
@@ -62,5 +65,58 @@ final class AcmeServiceTest extends AsyncTestCase
         $this->assertNotEmpty($l2 = (string) $account->getUrl());
 
         $this->assertSame($l1, $l2);
+    }
+
+    /**
+     * @test
+     */
+    public function issuance(): void
+    {
+        $domains = ['example.com'];
+
+        $this->service->register(null, true);
+        $order = $this->service->newOrder($domains);
+
+        foreach ($order->getAuthorizationUrls() as $authorizationUrl) {
+            $authorization = $this->service->getAuthorization($authorizationUrl);
+
+            if ($authorization->getIdentifier()->getType() !== 'dns') {
+                throw new AcmeException('Invalid identifier: ' . $authorization->getIdentifier()->getType());
+            }
+
+            $httpChallenge = $this->findHttpChallenge($authorization);
+
+            $this->service->finalizeChallenge($httpChallenge->getUrl());
+            $this->service->pollForAuthorization($authorization->getUrl());
+        }
+
+        $this->service->pollForOrderReady($order->getUrl());
+
+        $key = (new RsaKeyGenerator(2048))->generateKey();
+        $csr = (new OpensslCsrGenerator)->generateCsr($key, $domains);
+
+        $this->service->finalizeOrder($order->getFinalizationUrl(), $csr);
+        $this->service->pollForOrderValid($order->getUrl());
+
+        $order = $this->service->getOrder($order->getUrl());
+
+        $certificates = $this->service->downloadCertificates($order->getCertificateUrl());
+
+        self::assertCount(2, $certificates);
+        self::assertEquals('example.com', $certificates[0]->getSubject()->getCommonName());
+        self::assertStringStartsWith('Pebble Intermediate CA', $certificates[1]->getSubject()->getCommonName());
+    }
+
+    private function findHttpChallenge(Authorization $authorization): ?Challenge
+    {
+        $challenges = $authorization->getChallenges();
+
+        foreach ($challenges as $challenge) {
+            if ($challenge->getType() === 'http-01') {
+                return $challenge;
+            }
+        }
+
+        return null;
     }
 }
