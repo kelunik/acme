2cf6c500046625e50379c8fc74a776d60b6ab67e
Niklas Keller <me@kelunik.com>
2026-05-09 12:48:40 +0200

    Assert names instead of subject
    Modern validation is moving toward SAN-only checks. CN usage is being deprecated.
    
    See also: https://www.sectigo.com/knowledge-base/detail/64-Character-Limitation-on-Common-Name-CN-Field-in-X-509-Certificates

--- test/AcmeServiceTest.php
+++ test/AcmeServiceTest.php
@@ -103,7 +103,7 @@ final class AcmeServiceTest extends AsyncTestCase
         $certificates = $this->service->downloadCertificates($order->getCertificateUrl());
 
         self::assertCount(2, $certificates);
-        self::assertEquals('example.com', $certificates[0]->getSubject()->getCommonName(), $certificates[0]->toPem());
+        self::assertEquals(['example.com'], $certificates[0]->getNames(), $certificates[0]->toPem());
         self::assertStringStartsWith('Pebble Intermediate CA', $certificates[1]->getSubject()->getCommonName(), $certificates[0]->toPem());
     }
 
