7a1e9c8c77517b60c904e47ed6a9ad7dfe117a9b
Niklas Keller <me@kelunik.com>
2026-05-09 12:29:13 +0200

    Update pebble to ghcr.io/letsencrypt/pebble:latest

--- test/.run-pebble.sh
+++ test/.run-pebble.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker pull letsencrypt/pebble
-docker run --platform linux/amd64 -d --rm -e 'PEBBLE_VA_NOSLEEP=1' -e 'PEBBLE_VA_ALWAYS_VALID=1' -p 15000:15000 -p 14000:14000 --name pebble letsencrypt/pebble
+docker pull ghcr.io/letsencrypt/pebble:latest
+docker run --platform linux/amd64 -d --rm -e 'PEBBLE_VA_NOSLEEP=1' -e 'PEBBLE_VA_ALWAYS_VALID=1' -p 15000:15000 -p 14000:14000 --name pebble ghcr.io/letsencrypt/pebble:latest
